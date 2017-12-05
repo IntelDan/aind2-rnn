4c37ca6acd149354fdf5d50a521f61e08be250e0
IntelDan <intelligence.dace@gmail.com>
2017-12-05 23:42:25 +0800

    finish udacity work

--- my_answers.py
+++ my_answers.py
@@ -1,19 +1,23 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Activation
 from keras.layers import LSTM
 import keras
+import string
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
 
-    # reshape each 
+    for i in np.arange(0, len(series)-window_size, 1):
+        X.append(series[i:i+window_size])
+    y=series[window_size:]
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
@@ -23,12 +27,18 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
-
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    return model
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    ascii_chars = string.ascii_lowercase
+    for i in text:
+        if i not in punctuation and i not in ascii_chars:
+                text = text.replace(i,' ')
 
     return text
 
@@ -38,9 +48,18 @@ def window_transform_text(text, window_size, step_size):
     inputs = []
     outputs = []
 
+    for i in range(0, len(text)-window_size, step_size):
+        inputs.append(text[i:i+window_size])
+        outputs.append(text[i+window_size])
+
     return inputs,outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+# TODO build the required RNN model:
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars, activation='linear'))
+    model.add(Activation('softmax'))
+
+    return model
